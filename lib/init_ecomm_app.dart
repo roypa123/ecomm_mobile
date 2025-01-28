2fdb6d50ccbaa1637a899b3d5bc2d986b4c7c9fa
Roy P A <roy@nintrivalabs.com>
2025-01-28 17:02:01 +0530

    fix: some changes

--- lib/init_ecomm_app.dart
+++ lib/init_ecomm_app.dart
@@ -1,3 +1,5 @@
+import 'package:ecomm_mobile/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,6 +9,9 @@ import 'ecomm_bloc_observer.dart';
 Future<void> initEcommApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = const EcommBlocObserver();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await injectionContainer();
   await initiliazeObjects();
   await ScreenUtil.ensureScreenSize();
